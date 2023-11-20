855cee6edf482c8fcb16e4d30cb9d905e2a36d34
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-20 12:23:20 +0100

    1

--- ML/2_logistic_regression.py
+++ ML/2_logistic_regression.py
@@ -21,4 +21,19 @@ X = df.iloc[:, :-1]
 y = df.outcome
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = Lo
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+df1 = df.query('outcome==0').sample(n=500)
+df2 = df.query('outcome==1').sample(n=500)
+df3 = pd.concat([df1, df2])
+
+X = df3.iloc[:, :-1]
+y = df3.outcome
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
