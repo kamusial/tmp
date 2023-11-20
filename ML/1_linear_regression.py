7359b9bf8ed05f86d7c69f90855f95eb604591c2
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-20 10:49:47 +0100

    1

--- ML/1_linear_regression.py
+++ ML/1_linear_regression.py
@@ -1,8 +1,18 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('weight-height.csv')
 print(df)
 print(type(df))
 print(df.Gender.value_counts())
+
+df.Height *= 2.54
+df.Weight /= 2.2
+print('Units changed')
+print(df)
+sns.histplot(df.Weight)
+plt.show()
+
 
