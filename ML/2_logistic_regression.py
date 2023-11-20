d64d97a359a58483c78eae3a4e89af1f6a569a50
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-20 12:11:46 +0100

    1

--- ML/2_logistic_regression.py
+++ ML/2_logistic_regression.py
@@ -1,5 +1,24 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv('diabetes.csv')
 print(df.head().to_string())
 print(df.describe().T.to_string())
+print(df.isna().sum())
+print(df.outcome.value_counts())
+
+for col in ['glucose', 'bloodpressure', 'skinthickness', 'insulin', 'bmi', 'diabetespedigreefunction', 'age']:
+    df[col].replace(0, np.NaN, inplace=True)
+    mean_ = df[col].mean()
+    df[col].replace(np.NaN, mean_, inplace=True)
+print(df.isna().sum())
+print(df.describe().T.to_string())
+
+X = df.iloc[:, :-1]
+y = df.outcome
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = Lo
