7c8bcca9fa9b8c8c0f52d505b48c707084df83d9
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-04 11:05:16 +0100

    1

--- ML/3_KNN.py
+++ ML/3_KNN.py
@@ -22,3 +22,15 @@ sample = np.array([5.6, 3.2, 5.2, 1.45])
 sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
 plt.scatter(5.2, 1.45, c='r')
 plt.show()
+
+df['distance'] = (df.sepallength - sample[0]) ** 2 + (df.sepalwidth - sample[1]) ** 2 + \
+                 (df.petallength - sample[2]) ** 2 + (df.petalwidth - sample[3]) ** 2
+print(df.sort_values('distance').head(5).to_string())
+
+X = df.iloc[:, :4]
+y = df.class_values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = KNeighborsClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
