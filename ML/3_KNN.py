f1fe70482b79476e2e49feadc56d3350cbcecb7e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-04 11:21:39 +0100

    1

--- ML/3_KNN.py
+++ ML/3_KNN.py
@@ -27,10 +27,10 @@ df['distance'] = (df.sepallength - sample[0]) ** 2 + (df.sepalwidth - sample[1])
                  (df.petallength - sample[2]) ** 2 + (df.petalwidth - sample[3]) ** 2
 print(df.sort_values('distance').head(5).to_string())
 
-X = df.iloc[:, :4]
+X = df.iloc[:, :2]
 y = df.class_values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = KNeighborsClassifier()
+model = KNeighborsClassifier(n_neighbors=20, weights='distance')
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
