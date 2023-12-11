2a7d864ebc780b54736ab974aae804c837e938d8
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-11 12:16:45 +0100

    1

---
+++ ML/6_SVM.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+from sklearn.svm import SVC  #clasifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
