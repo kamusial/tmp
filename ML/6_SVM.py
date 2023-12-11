04f28a5f380d31cbb54467f38d54994df26d3c66
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-11 12:53:21 +0100

    1

--- ML/6_SVM.py
+++ ML/6_SVM.py
@@ -8,12 +8,31 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
-X, y = make_circles(n_samples=1000, factor=0.2, noise=0.4)
+X, y = make_circles(n_samples=1000, factor=0.2, noise=0.3)
 plt.scatter(x=X[:, 0], y=X[:, 1], c=y)
 plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’} or callable, default=’rbf’
+
+print('\nLogistic regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier(7)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nDecision Tree Clasifier')
+model = DecisionTreeClassifier(max_depth=4, min_samples_split=4)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nSVC')
 model = SVC(kernel='rbf')
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
